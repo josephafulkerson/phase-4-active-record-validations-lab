3af299fd102271ce00f900822c11e857ad0dcbfe
Joseph Fulkerson <josephafulkerson@gmail.com>
2021-11-04 12:41:35 -0400

    commit tree 4e432a570498988adc624df60d78ba430585bbd0

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+    validate :title, clickbait
+
+    def clickbait(event)
+        exclusion: {in: ["True Facts"]}
+    end
 end
